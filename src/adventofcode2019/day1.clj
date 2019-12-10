4cd50ae3dfa209a5405c2551f70d88023a292fd0
Johnny Eradus <jeradus@gmail.com>
2019-12-10 07:54:08 +0100

    Complete day2

--- src/adventofcode2019/day1.clj
+++ src/adventofcode2019/day1.clj
@@ -21,3 +21,26 @@
   (reduce + (map calculate-fuel xs)))
 
 
+
+(defn better-calculate-fuel [i]
+  (let [x (calculate-fuel i)]
+    (if (<= x 0)
+      0
+      (+ x (better-calculate-fuel x)))))
+
+(defn better-calculate-fuel-recursion
+  ([acc i]
+   (let [x (calculate-fuel i)]
+     (if (<= x 0)
+       acc
+       (recur (+ acc x) x))))
+  ([i]
+   (better-calculate-fuel-recursion 0 i)))
+
+
+(def ys (map to-int (str/split (slurp "resources/day1.1.txt") #"\n")))
+
+(defn day1.1 []
+    (reduce + (map better-calculate-fuel-recursion ys)))
+
+
--- src/adventofcode2019/day2.clj
+++ src/adventofcode2019/day2.clj
@@ -6,36 +6,67 @@
 (defn to-int [x]
   (Integer. x))
 
+(defn split-into-vector [s]
+  (into [] (map to-int (str/split s #","))))
 
-(def xs (map to-int (str/split (slurp "resources/day2.txt") #"\n")))
+(def xs (into [] (map to-int (str/split (slurp "resources/day2/1.txt") #","))))
 
-(defn round-down [d]
-  (Math/round (Math/floor d)))
-
-(defn calculate-fuel [i]
-  (let [x (round-down (/ i 3))]
-    (- x 2)))
-
-
-
-(defn better-calculate-fuel [i]
-  (let [x (calculate-fuel i)]
-    (if (<= x 0)
-      0
-      (+ x (better-calculate-fuel x)))))
-
-(defn better-calculate-fuel-recursion
-  ([acc i]
-   (let [x (calculate-fuel i)]
-     (if (<= x 0)
-       acc
-       (recur (+ acc x) x))))
-  ([i]
-   (better-calculate-fuel-recursion 0 i)))
+(defn computer [xs]
+  (reduce
+    (fn [xs [opt n1 n2 output]]
+      (let [opt (nth xs opt)]
+        (if (= opt 99)
+          (reduced xs)
+          (let [number1 (nth xs (nth xs n1))
+                number2 (nth xs (nth xs n2))
+                output-pos (nth xs output)
+                result (cond
+                         (= opt 1)
+                         (+ number1 number2)
+                         (= opt 2)
+                         (* number1 number2))]
+            (assoc xs output-pos result)))))
+    xs
+    (partition 4 (range 0 (count xs)))))
 
 
+(defn part-1 []
+  (computer (assoc xs 1 12 2 2)))
 
-(defn day2 []
-  (reduce + (map better-calculate-fuel-recursion xs)))
+
+
+
+(defn part-2
+  "Alternatively you create a list of pairs with all possible values.
+  This however is more fun :) "
+  []
+  (loop [noun (rand-int 100)
+         verb (rand-int 100)]
+    (if (= 19690720 (first (computer (assoc xs 1 noun 2 verb))))
+      {:noun   noun
+       :verb   verb
+       :result (+ (* 100 noun)
+                 verb)}
+      (recur (rand-int 100) (rand-int 100)))))
+
+(defn part-2-alternative []
+  (let [pos (mapcat
+              (fn [x]
+                (map
+                  (fn [y]
+                    (list x y))
+                  (range 100)))
+              (range 100))]
+    (reduce
+      (fn [_acc [noun verb]]
+        (when (= 19690720 (first (computer (assoc xs 1 noun 2 verb))))
+          (reduced {:noun   noun
+                    :verb   verb
+                    :result (+ (* 100 noun)
+                              verb)})))
+      nil
+      pos)))
+
+
 
 
